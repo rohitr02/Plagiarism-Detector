98f9d5e641f1f01eeb4b068a598c5d1844da9622
Pauli <paulipspd@gmail.com>
2021-04-15 02:46:59 -0400

    jsd integrated

--- compare.h
+++ compare.h
@@ -10,6 +10,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <stdatomic.h>
+#include <math.h>
 
 #ifndef DEBUG
 #define DEBUG true // CHANGE THIS TO FALSE BEFORE SUBMITTING
@@ -465,6 +466,7 @@ typedef struct word {
     char* word;
     double occurence;
     double frequency;
+    double average;
     struct word* next;
 } word;
 
@@ -845,7 +847,6 @@ void* readFile(void* arguments){
 }
 
 
-
 typedef struct jsdVals {
     double value;
     char* file1;
@@ -872,6 +873,116 @@ void* runAnalysis(void* arguments){
     // Loops thru the start index and end index
     for(int i = args->startIndex; i < args->endIndex; i++){
         printf("\tThis thread is scanning the files: %s and %s\n", args->array[i].file1, args->array[i].file2);
+        struct word* ptri = args->array[i].file1_LL;
+        struct word* ptrj = args->array[i].file2_LL;
+        //printf("%s\n", ptrj->word);
+        while((ptri != NULL) || (ptrj != NULL)) {
+            if(ptri == NULL) {
+                ptrj->average = ptrj->frequency * 0.5;
+                //printf("file2: %s\n", ptrj->word);
+                //printf("%f\n", ptrj->average);
+                ptrj = ptrj->next;
+                continue;
+            }
+            if(ptrj == NULL) {
+                ptri->average = ptri->frequency * 0.5;
+                //printf("file1: %s\n", ptri->word);
+                ptri = ptri->next;
+                continue;
+            }
+            char* currWord_filei = ptri->word;
+            char* currWord_filej = ptrj->word;
+            if(strcmp(currWord_filei, currWord_filej) == 0) {
+                double totalFrequency = ptri->frequency + ptrj->frequency;
+                double avg = totalFrequency * 0.5;
+                ptri->average = avg;
+                ptrj->average = avg;
+                ptri = ptri->next;
+                ptrj = ptrj->next;
+                continue;
+            }
+
+            char firstChar_i = currWord_filei[0];
+            char firstChar_j = currWord_filej[0];
+            if(firstChar_i < firstChar_j) {
+                //printf("file1: %s\n", ptri->word);
+                ptri->average = ptri->frequency * 0.5;;
+                //printf("%f\n", ptri->average);
+                ptri = ptri->next;
+                continue;
+            }
+
+            if(firstChar_j < firstChar_i) {
+                //printf("file2: %s\n", ptrj->word);
+                ptrj->average = ptrj->frequency * 0.5;;
+                ptrj = ptrj->next;
+                continue;
+            }
+
+            int currWord_filei_size = strlen(currWord_filei);
+            int currWord_filej_size = strlen(currWord_filej);
+            int minSize = currWord_filei_size < currWord_filej_size ? currWord_filei_size : currWord_filej_size;
+
+            char* ptri_word = currWord_filei;
+            char* ptrj_word = currWord_filej;
+
+            while(*ptri_word != '\0' && *ptrj_word != '\0') {
+                if(*ptri_word < *ptrj_word) {
+                    ptri->average = ptri->frequency * 0.5;
+                    ptri = ptri->next;
+                    break;
+                }
+                if(*ptrj_word < *ptri_word) {
+                    ptrj->average = ptrj->frequency * 0.5;
+                    ptrj = ptrj->next;
+                    break;
+                }
+                ptri_word++;
+                ptrj_word++;
+            }
+
+            if(*ptri_word == '\0' || *ptrj_word == '\0') {
+                if(minSize == currWord_filei_size) {
+                    ptri->average = ptri->frequency * 0.5;
+                    ptri = ptri->next;
+                }
+                else {
+                    ptrj->average = ptrj->frequency * 0.5;
+                    ptrj = ptrj->next;
+                }
+            }
+        }
+
+        
+        double filei_KLD = 0;
+        ptri = args->array[i].file1_LL;
+        while(ptri != NULL) {
+            //printf("wordi: %s\tfrequencyi: %f\tavaeragei: %f\tlog2i: %f\n",ptri->word, ptri->frequency,ptri->average, log_2i);
+            double x = (ptri->frequency/ptri->average);
+            filei_KLD = filei_KLD + (ptri->frequency * log2(x));
+            //printf("KLDi: %f\n", filei_KLD);
+            ptri = ptri->next;
+        }
+        //printf("KLD1: %f\n",filei_KLD);
+        //putchar('\n');
+
+        double filej_KLD = 0;
+        ptrj = args->array[i].file2_LL;
+        while(ptrj != NULL) {
+            //printf("wordj: %s\tfrequencyj: %f\tavaeragej: %f\tlog2j: %f\n",ptrj->word, ptrj->frequency,ptrj->average, log_2j);
+            double x = (ptrj->frequency/ptrj->average);
+            filej_KLD = filej_KLD + (ptrj->frequency * log2(x));
+            //printf("KLDj: %f\n", filej_KLD);
+            ptrj = ptrj->next;
+        }
+        //printf("KLD2: %f\n",filej_KLD);
+
+        double filei_jsd = filei_KLD * 0.5;
+        double filej_jsd = filej_KLD * 0.5;
+        double jsd = sqrt(filei_jsd + filej_jsd);
+        //printf("%f\n", jsd);
+        args->array[i].value = jsd;
+
     }
     return NULL;
 }
